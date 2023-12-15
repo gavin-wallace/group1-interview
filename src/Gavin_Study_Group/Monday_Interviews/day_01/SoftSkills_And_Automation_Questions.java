996b5aa19428e951c457bc6ed5aa92184c9c593e
AslanAslanli <143632955+AslanAslanli@users.noreply.github.com>
2023-12-16 01:47:05 +0400

    Commit

---
+++ src/Gavin_Study_Group/Monday_Interviews/day_01/SoftSkills_And_Automation_Questions.java
@@ -0,0 +1,14 @@
+package Gavin_Study_Group.Monday_Interviews.day_01;
+
+public class SoftSkills_And_Automation_Questions {
+    /*
+    1. when was the last time you found a bug that got rejected by the Test Lead?
+    2. what is ur most recent bug?
+    3. talk to me about your current project, what do you like and dislike about it?
+
+
+    Automation:
+    2. locators; xpath, css?
+    3. https://www.azal.az/en/index.html --> get the "Flight date" text message and print it our on the screen
+     */
+}
