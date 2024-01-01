c931db3f6ee0ad6c9eb174d8083260a64f24eefa
primarypc <gwwlax@gmail.com>
2024-01-01 15:25:26 -0500

    Gavin Commit - Jan 01, 2024

--- src/test/java/Gavin_Study_Group/Interview_Jan01/Automation.java
+++ src/test/java/Gavin_Study_Group/Interview_Jan01/Automation.java
@@ -1,5 +1,17 @@
 package Gavin_Study_Group.Interview_Jan01;
 
+import Gavin_Study_Group.Interview_Jan01.Pages.DocuportMyUploadsPage;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static Gavin_Study_Group.Interview_Jan01.Pages.DocuportMyUploadsPage.*;
+import static Utilities.ConfigurationReader.*;
+import static Utilities.Driver.*;
+import static java.time.Duration.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+import static org.testng.Assert.*;
+
 public class Automation {
     /*
     1. Explain the difference among Selenium, Driver, Bonigarcia, Maven, POM file and Browser.
@@ -8,4 +20,30 @@ public class Automation {
     4. Go to beta.docuport.app website. Login as a Client. From My Uploads take the table
         headers and put them into a list. And do this all using POM model
      */
+
+    WebDriverWait wait;
+
+    @Test
+    public void name (){
+        getUsername().sendKeys(USERNAME_ClIENT);
+        getPassword().sendKeys(PASSWORD);
+        getLoginButton().click();
+
+        wait.until(elementToBeClickable(getContinueButton())).click();
+        wait.until(elementToBeClickable(getThreeLines())).click();
+        wait.until(elementToBeClickable(getMyUploads())).click();
+
+        //List<WebElement> listOfTableHeaders = DocuportMyUploadsPage.getTableHeaders();
+        System.out.println(getTableHeaders().size());
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        getDriver();
+        getDriver().get(getProperty("docuport.url"));
+        new DocuportMyUploadsPage();
+        wait = new WebDriverWait(getDriver(), ofSeconds(10));
+        assertTrue(getDriver().getTitle().contains("Docuport"));
+
+    }
 }
---
+++ src/test/java/Utilities/Driver.java
@@ -0,0 +1,38 @@
+package Utilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
+
+public class Driver {
+    private Driver(){}
+    private static WebDriver driver;
+
+    public static WebDriver getDriver (){
+        if (driver==null) {
+            String browserType = ConfigurationReader.getProperty("browser");
+            switch (browserType.toLowerCase()){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+            }
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        return driver;
+    }
+
+    public static void closeDriver(){
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+
+
+}
