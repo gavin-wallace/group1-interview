9465718c309f1ffb7ee445c08f8b6e1082663f42
primarypc <gwwlax@gmail.com>
2024-01-08 22:56:07 -0500

    Gavin Commit - Jan 8, 2024

---
+++ src/test/java/aslan_interviews/interview_prep_jan07/Automation_POM_Task.java
@@ -0,0 +1,20 @@
+package aslan_interviews.interview_prep_jan07;
+/*
+task
+=============
+
+    1. go to https://loopcamp.vercel.app/
+    2. click "Multiple Windows" link
+    3. Validate "Powered by Loopcamp" is displayed
+    4. Validate title is "Windows"
+    5. Click - click here
+    6. Switch to new window and validate title
+
+GOAL: Create and implement Driver class, configuration.properties, ConfigurationReader, bowserUtils, and LoopCampPracticeMultipleWindowsPage
+Explain/Demonstrate: POM and Singleton Pattern
+
+
+ */
+public class Automation_POM_Task {
+
+}
---
+++ src/test/java/aslan_interviews/interview_prep_jan07/Java_Task.java
@@ -0,0 +1,16 @@
+package aslan_interviews.interview_prep_jan07;
+
+
+public class Java_Task {
+
+    //Task01 (bool logic) -> https://codingbat.com/prob/p137742
+
+    // or:
+
+    //Task02 (String manipulation) -> https://codingbat.com/prob/p165312
+
+    // or:
+
+    //Task03 (Map) -> https://codingbat.com/prob/p115011
+
+}
